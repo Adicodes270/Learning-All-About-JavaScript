24d75d3f98a3c3bf84add04ab444f42202cd1df4
Adicodes270 <adityaalt9090@gmail.com>
2026-01-17 22:57:03 +0530

    Added Comments Folder

---
+++ COMMENTS/script.js
@@ -0,0 +1,7 @@
+console.log("hello world!");
+
+// This is a single line comment it is not executed 
+// It is used to typically describe our code
+
+/* This is a multi line comment it is also not executed 
+it is used to typically describe large chunk of code  */
