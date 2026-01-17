26b9a45c74976ebd75746d047358457387007810
Adicodes270 <adityaalt9090@gmail.com>
2026-01-17 23:20:03 +0530

    Did unary operator and artimetic operator

---
+++ OPERATORS/script.js
@@ -0,0 +1,19 @@
+// Aritmetic Operators
+let a = 5;
+let b = 2;
+
+console.log("a =", a , " & b =", b);
+console.log("a + b =", a + b);
+console.log("a - b =", a - b);
+console.log("a * b =", a * b);
+console.log("a / b =", a / b);
+console.log("a % b =", a % b); // Gives remainder and is called modulus
+console.log("a ** b =", a**b); /* Exponentation like 5 to power 
+of 2 */
+
+// Unary Operators 
+a++; // Is called increment and increases value by 1 
+a--; // Is called decrement and decreases value by 1
+console.log(a);
+/* ++a and --a are also valid and are called pre increment/
+decrement and a++ and a-- are called post increment / decrement
