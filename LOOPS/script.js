3eb2b37e3120147d5d6c2343978b10f7076dac2a
Adicodes270 <adityaalt9090@gmail.com>
2026-01-21 22:13:14 +0530

    Break from coding

---
+++ LOOPS/script.js
@@ -0,0 +1,25 @@
+// Types of loop - 
+// 1. For Loop
+// 2. While Loop
+// 3. Do While Loop
+
+// For Loop
+
+
+for (let num = 1; num<=5; num++){  // first line is initilization second is stopping
+    // Third is updation
+    console.log("For loops") // 5 times execute
+}
+
+console.log("Loop ended");
+
+
+// Calculate sum of 1 to 10
+
+let sum = 0;
+
+for (let i=1; i<=10; i++){
+    sum = sum + i
+}
+
+console.log("sum = ",sum)
