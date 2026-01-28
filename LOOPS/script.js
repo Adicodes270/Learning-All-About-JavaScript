3e0550848181e0f550779695545012398246b5bb
Adicodes270 <adityaalt9090@gmail.com>
2026-01-28 19:29:00 +0530

    Loops in JS Updated

--- LOOPS/script.js
+++ LOOPS/script.js
@@ -8,10 +8,10 @@
 
 for (let num = 1; num<=5; num++){  // first line is initilization second is stopping
     // Third is updation
-    console.log("For loops") // 5 times execute
+    console.log("For loops",num)
 }
 
-console.log("Loop ended");
+// console.log("Loop ended",num);
 
 
 // Calculate sum of 1 to 10
@@ -23,3 +23,31 @@ for (let i=1; i<=10; i++){
 }
 
 console.log("sum = ",sum)
+
+// for ( let i = 1; i>=0; i++) {
+//     console.log(i);
+// }
+
+// this is a infinite loop never do this in your code
+
+// While Loop
+
+let i = 1;
+while (i>=5) {
+    console.log(i);
+    i++;
+}
+
+// Do-while loop
+
+let a = 20;
+
+do {
+    console.log("Hello");
+    a++;
+} while (a<= 10);
+
+// Runs atleast once and checks condition after running code 
+// At last when we write while after writing condition we have to add ;
+
+// For-of Loop
