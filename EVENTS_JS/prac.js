cc7b5f166585b286714eafd3dc47eecb8390b167
Adicodes270 <adityaalt9090@gmail.com>
2026-03-23 14:15:04 +0530

    PROPER WORKING OF TOGGLE BUTTON DONE

--- EVENTS_JS/prac.js
+++ EVENTS_JS/prac.js
@@ -1,20 +1,16 @@
 const toggleButton = document.querySelector(".toggle-button");
+const body = document.querySelector("body");
 
 
+toggleButton.addEventListener("click", () => {
 
-toggleButton.addEventListener("click", () =>{
+    body.classList.toggle('dark');
 
-    let mode = toggleButton.getAttribute("current-value");
-    if (mode==="light"){
-        toggleButton.setAttribute("current-value", "dark")
-        document.body.style.backgroundColor="white";
-    }
-    else if (mode==="dark"){
-        toggleButton.setAttribute("current-value", "light")
-        document.body.style.backgroundColor="black";
-    }
-        
     
+    const isDark = body.classList.contains('dark');
+    toggleButton.setAttribute("current-value", isDark ? "dark" : "light");
+
+
 });
 
 
